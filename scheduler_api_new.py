88c989efb79b93021c61e604659dfe7e91c7e1c9
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-09-09 12:57:35 -0300

    comment signal

--- scheduler_api_new.py
+++ scheduler_api_new.py
@@ -257,8 +257,8 @@ def signal_handler(signum, frame):
     scheduler.shutdown(wait=True)
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+# signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGTERM, signal_handler)
 
 # -------------------
 # Run server
